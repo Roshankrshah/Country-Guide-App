765dd2e9507aa8e4f159ce3321e9c1439ee34cb2
Roshan <shahroshan0720@gmail.com>
2023-07-10 20:15:14 +0530

    add search by name funtion

--- app.js
+++ app.js
@@ -25,44 +25,43 @@ btnContainer.addEventListener('click', (e) => {
 
 const result = document.querySelector(".result");
 
-function fixedTemp(data) {
+function fixedTemp(data, idx) {
     return `
-    <img src="${data[0].flags.svg}" alt="data[0].flags.alt" class="flag-img" />
-    <h2>${data[0].name.common}</h2>
+    <img src="${data[idx].flags.svg}" alt="data[0].flags.alt" class="flag-img" />
+    <h2>${data[idx].name.common}</h2>
     <div class="wrapper">
         <div class="data-wrapper">
             <h4>Capital:</h4>
-            <span>${data[0].capital[0]}</span>
+            <span>${data[idx].capital[0]}</span>
         </div>
     </div>
     <div class="wrapper">
         <div class="data-wrapper">
             <h4>Continent:</h4>
-            <span>${data[0].continents[0]}</span>
+            <span>${data[idx].continents[0]}</span>
         </div>
     </div>
     <div class="wrapper">
         <div class="data-wrapper">
             <h4>Population:</h4>
-            <span>${data[0].population}</span>
+            <span>${data[idx].population}</span>
         </div>
     </div>
     <div class="wrapper">
         <div class="data-wrapper">
             <h4>Currency:</h4>
-            <span>${data[0].currencies[Object.keys(data[0].currencies)[0]].name} - ${Object.keys(data[0].currencies)[0]}</span>
+            <span>${data[idx].currencies[Object.keys(data[idx].currencies)[0]].name} - ${Object.keys(data[idx].currencies)[0]}</span>
         </div>
     </div>
     <div class="wrapper">
         <div class="data-wrapper">
             <h4>Common Languages:</h4>
-            <span>${Object.values(data[0].languages).toString().split(',').join(', ')}</span>
+            <span>${Object.values(data[idx].languages).toString().split(',').join(', ')}</span>
         </div>
     </div>`;
 }
 
 searchBtn1.addEventListener("click", () => {
-    console.log("mia khalifa");
     const countryInp = document.getElementById("country-inp");
     const inputValue = countryInp.value;
     const finalUrl = `https://restcountries.com/v3.1/name/${inputValue}?fullText=true`;
@@ -87,52 +86,41 @@ searchBtn1.addEventListener("click", () => {
 });
 
 searchBtn2.addEventListener("click", () => {
-    console.log("Anglia White");
     const nameInp = document.getElementById("name-inp");
     const inputValue = nameInp.value;
     const finalUrl = `https://restcountries.com/v3.1/name/${inputValue}`;
 
     fetch(finalUrl).then((response) => response.json()).then((data) => {
-        console.log(data.length);
-        /*console.log(data[0].flags.png);
+        /*console.log(data.length);
+        console.log(data[0].flags.png);
         console.log(Object.keys(data[0].currencies)[0]);
         console.log(Object.values(data[0].languages).toString().split(',').join(', '))*/
 
-        let displayDet = `
-        <img src="${data[0].flags.svg}" alt="data[0].flags.alt" class="flag-img" />
-        <h2>${data[0].name.common}</h2>
-        <div class="wrapper">
-            <div class="data-wrapper">
-                <h4>Capital:</h4>
-                <span>${data[0].capital[0]}</span>
-            </div>
-        </div>
-        <div class="wrapper">
-            <div class="data-wrapper">
-                <h4>Continent:</h4>
-                <span>${data[0].continents[0]}</span>
-            </div>
-        </div>
-        <div class="wrapper">
-            <div class="data-wrapper">
-                <h4>Population:</h4>
-                <span>${data[0].population}</span>
-            </div>
-        </div>
-        <div class="wrapper">
-            <div class="data-wrapper">
-                <h4>Currency:</h4>
-                <span>${data[0].currencies[Object.keys(data[0].currencies)[0]].name} - ${Object.keys(data[0].currencies)[0]}</span>
-            </div>
-        </div>
-        <div class="wrapper">
-            <div class="data-wrapper">
-                <h4>Common Languages:</h4>
-                <span>${Object.values(data[0].languages).toString().split(',').join(', ')}</span>
-            </div>
-        </div>`
+        const countries = [];
+        for (let i = 0; i < data.length; i++) {
+            countries.push(i);
+        }
+        const countryBtns = countries
+            .map(function (country) {
+                return `<button type="button" class="filter-btn" data-id=${country}>
+            ${data[country].name.common}
+          </button>`;
+            })
+            .join("");
 
-        result.innerHTML = displayDet;
+        result.innerHTML = countryBtns;
+
+        const filterBtns = result.querySelectorAll(".filter-btn");
+        filterBtns.forEach((btn) => {
+            btn.addEventListener("click", (e) => {
+                const category = e.currentTarget.dataset.id;
+                const idx = parseInt(category);
+                let displayDet = fixedTemp(data, idx);
+                result.innerHTML = displayDet;
+            })
+        })
+
+
 
     }).catch(() => {
         if (inputValue.length == 0) {
